29d09e04a1bb7247c1d806611fd9ec7f7a7a6595
Elias-Zurita <eliass.zurita@gmail.com>
2024-05-06 13:43:52 -0300

    efectos listo

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ window.addEventListener('scroll', function() {
         headerDesktop.classList.remove('headerScrolled');
     }
 });
+
+
+// Back to top button
+
+const backToTopButton = document.querySelector('.back-to-top');
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
